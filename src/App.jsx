82ad915cd0c0598ed6908e2227c446a576b432c3
ytraiba <ytraiba@umass.edu>
2022-08-29 11:34:33 -0400

    Update
    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,93 @@ import {React, useState} from 'react'
 import './App.css'
 import { MapContainer, TileLayer,  GeoJSON, Marker, Popup } from "react-leaflet";
 import * as L from "leaflet";
-import * as states from "./states.json"
+import { states } from "./states"
 import { popups } from "./popups"
 import Logo from './images/logo-infrasense-300x200.png'
-
 
 const mapIcon = new L.icon({
   iconUrl: require("./images/marker.png"),
   iconSize: [20, 20]
 });
 
+// const states_index = { Alabama: 1,
+//   'Alaska': 2,
+//   'Arizona': 3,
+//   'Arkansas': 4,
+//   'California': 5,
+//   'Colorado': 6,
+//   'Connecticut': 7,
+//   'Delaware': 8,
+//   'Florida': 9,
+//   'Georgia': 10,
+//   'Hawaii': 11,
+//   'Idaho': 12,
+//   'Illinois': 13,
+//   'Indiana': 14,
+//   'Iowa': 15,
+//   'Kansas': 16,
+//   'Kentucky': 17,
+//   'Louisiana': 18,
+//   'Maine': 19,
+//   'Maryland': 20,
+//   'Massachusetts': 21,
+//   'Michigan': 22,
+//   'Minnesota': 23,
+//   'Mississippi': 24,
+//   'Missouri': 25,
+//   'Montana': 26,
+//   'Nebraska': 27,
+//   'Nevada': 28,
+//   'New Hampshire': 29,
+//   'New Jersey': 30,
+//   'New Mexico': 31,
+//   'New York': 32,
+//   'North Carolina': 33,
+//   'North Dakota': 34,
+//   'Ohio': 35,
+//   'Oklahoma': 36,
+//   'Oregon': 37,
+//   'Pennsylvania': 38,
+//   'Rhode Island': 39,
+//   'South Carolina': 40,
+//   'South Dakota': 41,
+//   'Tennessee': 42,
+//   'Texas': 43,
+//   'Utah': 44,
+//   'Vermont': 45,
+//   'Virginia': 46,
+//   'Washington': 47,
+//   'West Virginia': 48,
+//   'Wisconsin': 49,
+//   'Wyoming': 50,
+//   'Puerto Rico': 51,
+//   'District of Columbia': 52,
+// }
 
 function App() {
   const [ activePopup, setActivePopup ] = useState( null );
 
   const mapPolygonColorToDensity=(density => {
-    return density > 600000
+    return density > 3000000
         ? '#a50f15'
-        : density > 200000
+        : density > 2000000
         ? '#de2d26'
-        : density > 100000
+        : density > 1000000
         ? '#fb6a4a'
-        : density > 50000
+        : density > 500000
         ? '#fc9272'
-        : density > 20000
+        : density > 100000
         ? '#fcbba1'
         : '#fee5d9';
     })
+  
+  
   const style = (feature => {
+      var index = popups.findIndex(obj => obj.State === feature.properties.NAME);
+      // console.log(popups[index].Population)
+
     return ({
-        fillColor: mapPolygonColorToDensity(feature.properties.CENSUSAREA),
+        fillColor: mapPolygonColorToDensity(popups[index].Population),
         weight: 1,
         opacity: 1,
         color: 'white',
@@ -59,6 +116,7 @@ function App() {
             icon= { mapIcon }
           />
        ))}
+       
 
       { activePopup && (
         <Popup 
@@ -82,13 +140,3 @@ function App() {
 }
 
 export default App;
-
-
-// import { Icon } from 'leaflet'
-// import covidData from './data.json'
-// import icon1 from './images/covid19.svg'
-
-// const covidIcon = new Icon({
-//   iconUrl: icon1,
-//   iconSize: [25, 25]
-// })
--- src/popups.js
+++ src/popups.js
@@ -1,252 +1,314 @@
 export const popups = [
- {
-   "State": "Wisconsin",
-   "Lat": 44.5,
-   "Long": -89.5
- },
- {
-   "State": "West Virginia",
-   "Lat": 39,
-   "Long": -80.5
- },
- {
-   "State": "Vermont",
-   "Lat": 44,
-   "Long": -72.699997
- },
- {
-   "State": "Texas",
-   "Lat": 31,
-   "Long": -100
- },
- {
-   "State": "South Dakota",
-   "Lat": 44.5,
-   "Long": -100
- },
- {
-   "State": "Rhode Island",
-   "Lat": 41.742325,
-   "Long": -71.742332
- },
- {
-   "State": "Oregon",
-   "Lat": 44,
-   "Long": -120.5
- },
- {
-   "State": "New York",
-   "Lat": 43,
-   "Long": -75
- },
- {
-   "State": "New Hampshire",
-   "Lat": 44,
-   "Long": -71.5
- },
- {
-   "State": "Nebraska",
-   "Lat": 41.5,
-   "Long": -100
- },
- {
-   "State": "Kansas",
-   "Lat": 38.5,
-   "Long": -98
- },
- {
-   "State": "Mississippi",
-   "Lat": 33,
-   "Long": -90
- },
- {
-   "State": "Illinois",
-   "Lat": 40,
-   "Long": -89
- },
- {
-   "State": "Delaware",
-   "Lat": 39,
-   "Long": -75.5
- },
- {
-   "State": "Connecticut",
-   "Lat": 41.599998,
-   "Long": -72.699997
- },
- {
-   "State": "Arkansas",
-   "Lat": 34.799999,
-   "Long": -92.199997
- },
- {
-   "State": "Indiana",
-   "Lat": 40.273502,
-   "Long": -86.126976
- },
- {
-   "State": "Missouri",
-   "Lat": 38.573936,
-   "Long": -92.60376
- },
- {
-   "State": "Florida",
-   "Lat": 27.994402,
-   "Long": -81.760254
- },
- {
-   "State": "Nevada",
-   "Lat": 39.876019,
-   "Long": -117.224121
- },
- {
-   "State": "Maine",
-   "Lat": 45.367584,
-   "Long": -68.972168
- },
- {
-   "State": "Michigan",
-   "Lat": 44.182205,
-   "Long": -84.506836
- },
- {
-   "State": "Georgia",
-   "Lat": 33.247875,
-   "Long": -83.441162
- },
- {
-   "State": "Hawaii",
-   "Lat": 19.741755,
-   "Long": -155.844437
- },
- {
-   "State": "Alaska",
-   "Lat": 66.160507,
-   "Long": -153.369141
- },
- {
-   "State": "Tennessee",
-   "Lat": 35.860119,
-   "Long": -86.660156
- },
- {
-   "State": "Virginia",
-   "Lat": 37.926868,
-   "Long": -78.024902
- },
- {
-   "State": "New Jersey",
-   "Lat": 39.833851,
-   "Long": -74.871826
- },
- {
-   "State": "Kentucky",
-   "Lat": 37.839333,
-   "Long": -84.27002
- },
- {
-   "State": "North Dakota",
-   "Lat": 47.650589,
-   "Long": -100.437012
- },
- {
-   "State": "Minnesota",
-   "Lat": 46.39241,
-   "Long": -94.63623
- },
- {
-   "State": "Oklahoma",
-   "Lat": 36.084621,
-   "Long": -96.921387
- },
- {
-   "State": "Montana",
-   "Lat": 46.96526,
-   "Long": -109.533691
- },
- {
-   "State": "Washington",
-   "Lat": 47.751076,
-   "Long": -120.740135
- },
- {
-   "State": "Utah",
-   "Lat": 39.41922,
-   "Long": -111.950684
- },
- {
-   "State": "Colorado",
-   "Lat": 39.113014,
-   "Long": -105.358887
- },
- {
-   "State": "Ohio",
-   "Lat": 40.367474,
-   "Long": -82.996216
- },
- {
-   "State": "Alabama",
-   "Lat": 32.31823,
-   "Long": -86.902298
- },
- {
-   "State": "Iowa",
-   "Lat": 42.032974,
-   "Long": -93.581543
- },
- {
-   "State": "New Mexico",
-   "Lat": 34.307144,
-   "Long": -106.018066
- },
- {
-   "State": "South Carolina",
-   "Lat": 33.836082,
-   "Long": -81.163727
- },
- {
-   "State": "Pennsylvania",
-   "Lat": 41.203323,
-   "Long": -77.194527
- },
- {
-   "State": "Arizona",
-   "Lat": 34.048927,
-   "Long": -111.093735
- },
- {
-   "State": "Maryland",
-   "Lat": 39.045753,
-   "Long": -76.641273
- },
- {
-   "State": "Massachusetts",
-   "Lat": 42.407211,
-   "Long": -71.382439
- },
- {
-   "State": "California",
-   "Lat": 36.778259,
-   "Long": -119.417931
- },
- {
-   "State": "Idaho",
-   "Lat": 44.068203,
-   "Long": -114.742043
- },
- {
-   "State": "Wyoming",
-   "Lat": 43.07597,
-   "Long": -107.290283
- },
- {
-   "State": "North Carolina",
-   "Lat": 35.782169,
-   "Long": -80.793457
- },
- {
-   "State": "Louisiana",
-   "Lat": 30.39183,
-   "Long": -92.329102
- }
+  {
+    "State": "Alabama",
+    "Lat": 32.31823,
+    "Long": -86.902298,
+    "Population": 5024279
+  },
+  {
+    "State": "Alaska",
+    "Lat": 66.160507,
+    "Long": -153.369141,
+    "Population": 733391
+  },
+  {
+    "State": "Arizona",
+    "Lat": 34.048927,
+    "Long": -111.093735,
+    "Population": 7151502
+  },
+  {
+    "State": "Arkansas",
+    "Lat": 34.799999,
+    "Long": -92.199997,
+    "Population": 3011524
+  },
+  {
+    "State": "California",
+    "Lat": 36.778259,
+    "Long": -119.417931,
+    "Population": 39538223
+  },
+  {
+    "State": "Colorado",
+    "Lat": 39.113014,
+    "Long": -105.358887,
+    "Population": 5773714
+  },
+  {
+    "State": "Connecticut",
+    "Lat": 41.599998,
+    "Long": -72.699997,
+    "Population": 3605944
+  },
+  {
+    "State": "Delaware",
+    "Lat": 39,
+    "Long": -75.5,
+    "Population": 989948
+  },
+  {
+    "State": "Florida",
+    "Lat": 27.994402,
+    "Long": -81.760254,
+    "Population": 21538187
+  },
+  {
+    "State": "Georgia",
+    "Lat": 33.247875,
+    "Long": -83.441162,
+    "Population": 10711908
+  },
+  {
+    "State": "Hawaii",
+    "Lat": 19.741755,
+    "Long": -155.844437,
+    "Population": 1455271
+  },
+  {
+    "State": "Idaho",
+    "Lat": 44.068203,
+    "Long": -114.742043,
+    "Population": 1839106
+  },
+  {
+    "State": "Illinois",
+    "Lat": 40,
+    "Long": -89,
+    "Population": 12812508
+  },
+  {
+    "State": "Indiana",
+    "Lat": 40.273502,
+    "Long": -86.126976,
+    "Population": 6785528
+  },
+  {
+    "State": "Iowa",
+    "Lat": 42.032974,
+    "Long": -93.581543,
+    "Population": 3190369
+  },
+  {
+    "State": "Kansas",
+    "Lat": 38.5,
+    "Long": -98,
+    "Population": 2937880
+  },
+  {
+    "State": "Kentucky",
+    "Lat": 37.839333,
+    "Long": -84.27002,
+    "Population": 4505836
+  },
+  {
+    "State": "Louisiana",
+    "Lat": 30.39183,
+    "Long": -92.329102,
+    "Population": 4657757
+  },
+  {
+    "State": "Maine",
+    "Lat": 45.367584,
+    "Long": -68.972168,
+    "Population": 1362359
+  },
+  {
+    "State": "Maryland",
+    "Lat": 39.045753,
+    "Long": -76.641273,
+    "Population": 6177224
+  },
+  {
+    "State": "Massachusetts",
+    "Lat": 42.407211,
+    "Long": -71.382439,
+    "Population": 7029917
+  },
+  {
+    "State": "Michigan",
+    "Lat": 44.182205,
+    "Long": -84.506836,
+    "Population": 10077331
+  },
+  {
+    "State": "Minnesota",
+    "Lat": 46.39241,
+    "Long": -94.63623,
+    "Population": 5706494
+  },
+  {
+    "State": "Mississippi",
+    "Lat": 33,
+    "Long": -90,
+    "Population": 2961279
+  },
+  {
+    "State": "Missouri",
+    "Lat": 38.573936,
+    "Long": -92.60376,
+    "Population": 6154913
+  },
+  {
+    "State": "Montana",
+    "Lat": 46.96526,
+    "Long": -109.533691,
+    "Population": 1084225
+  },
+  {
+    "State": "Nebraska",
+    "Lat": 41.5,
+    "Long": -100,
+    "Population": 1961504
+  },
+  {
+    "State": "Nevada",
+    "Lat": 39.876019,
+    "Long": -117.224121,
+    "Population": 3104614
+  },
+  {
+    "State": "New Hampshire",
+    "Lat": 44,
+    "Long": -71.5,
+    "Population": 1377529
+  },
+  {
+    "State": "New Jersey",
+    "Lat": 39.833851,
+    "Long": -74.871826,
+    "Population": 9288994
+  },
+  {
+    "State": "New Mexico",
+    "Lat": 34.307144,
+    "Long": -106.018066,
+    "Population": 2117522
+  },
+  {
+    "State": "New York",
+    "Lat": 43,
+    "Long": -75,
+    "Population": 20201249
+  },
+  {
+    "State": "North Carolina",
+    "Lat": 35.782169,
+    "Long": -80.793457,
+    "Population": 10439388
+  },
+  {
+    "State": "North Dakota",
+    "Lat": 47.650589,
+    "Long": -100.437012,
+    "Population": 779094
+  },
+  {
+    "State": "Ohio",
+    "Lat": 40.367474,
+    "Long": -82.996216,
+    "Population": 11799448
+  },
+  {
+    "State": "Oklahoma",
+    "Lat": 36.084621,
+    "Long": -96.921387,
+    "Population": 3959353
+  },
+  {
+    "State": "Oregon",
+    "Lat": 44,
+    "Long": -120.5,
+    "Population": 4237256
+  },
+  {
+    "State": "Pennsylvania",
+    "Lat": 41.203323,
+    "Long": -77.194527,
+    "Population": 13002700
+  },
+  {
+    "State": "Rhode Island",
+    "Lat": 41.742325,
+    "Long": -71.742332,
+    "Population": 1097379
+  },
+  {
+    "State": "South Carolina",
+    "Lat": 33.836082,
+    "Long": -81.163727,
+    "Population": 5118425
+  },
+  {
+    "State": "South Dakota",
+    "Lat": 44.5,
+    "Long": -100,
+    "Population": 886667
+  },
+  {
+    "State": "Tennessee",
+    "Lat": 35.860119,
+    "Long": -86.660156,
+    "Population": 6910840
+  },
+  {
+    "State": "Texas",
+    "Lat": 31,
+    "Long": -100,
+    "Population": 29145505
+  },
+  {
+    "State": "Utah",
+    "Lat": 39.41922,
+    "Long": -111.950684,
+    "Population": 3271616
+  },
+  {
+    "State": "Vermont",
+    "Lat": 44,
+    "Long": -72.699997,
+    "Population": 643077
+  },
+  {
+    "State": "Virginia",
+    "Lat": 37.926868,
+    "Long": -78.024902,
+    "Population": 8631393
+  },
+  {
+    "State": "Washington",
+    "Lat": 47.751076,
+    "Long": -120.740135,
+    "Population": 7705281
+  },
+  {
+    "State": "West Virginia",
+    "Lat": 39,
+    "Long": -80.5,
+    "Population": 1793716
+  },
+  {
+    "State": "Wisconsin",
+    "Lat": 44.5,
+    "Long": -89.5,
+    "Population": 5893718
+  },
+  {
+    "State": "Wyoming",
+    "Lat": 43.07597,
+    "Long": -107.290283,
+    "Population": 5768511,
+  },
+  {
+    "State": "Puerto Rico",
+    "Lat": 18.2,
+    "Long": -66.66,
+    "Population": 5768511,
+  },
+  {
+    "State": "District of Columbia",
+    "Lat": 38.897,
+    "Long": -77.036,
+    "Population": 5768511,
+  }
 ]
