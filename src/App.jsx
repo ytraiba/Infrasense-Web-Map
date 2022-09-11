c78414256d7b9c4370763c0c7b41b017f76af38b
ytraiba <ytraiba@umass.edu>
2022-09-11 16:41:29 -0400

    2nd Draft
    All Data is accurate.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import { MapContainer, TileLayer,  GeoJSON, Marker, Popup } from "react-leaflet"
 import * as L from "leaflet";
 import { states } from "./states"
 import { popups } from "./popups"
+import  bridgeLogo  from './images/bridge.png'
+import  concreteLogo  from './images/concrete.png'
+import  consultingLogo  from './images/consulting.png'
+import  utilityLogo  from './images/utility.png'
+import  pavementLogo  from './images/pavement.png'
 import Logo from './images/logo-infrasense-300x200.png'
 
 const mapIcon = new L.icon({
@@ -11,84 +16,70 @@ const mapIcon = new L.icon({
   iconSize: [20, 20]
 });
 
-// const states_index = { Alabama: 1,
-//   'Alaska': 2,
-//   'Arizona': 3,
-//   'Arkansas': 4,
-//   'California': 5,
-//   'Colorado': 6,
-//   'Connecticut': 7,
-//   'Delaware': 8,
-//   'Florida': 9,
-//   'Georgia': 10,
-//   'Hawaii': 11,
-//   'Idaho': 12,
-//   'Illinois': 13,
-//   'Indiana': 14,
-//   'Iowa': 15,
-//   'Kansas': 16,
-//   'Kentucky': 17,
-//   'Louisiana': 18,
-//   'Maine': 19,
-//   'Maryland': 20,
-//   'Massachusetts': 21,
-//   'Michigan': 22,
-//   'Minnesota': 23,
-//   'Mississippi': 24,
-//   'Missouri': 25,
-//   'Montana': 26,
-//   'Nebraska': 27,
-//   'Nevada': 28,
-//   'New Hampshire': 29,
-//   'New Jersey': 30,
-//   'New Mexico': 31,
-//   'New York': 32,
-//   'North Carolina': 33,
-//   'North Dakota': 34,
-//   'Ohio': 35,
-//   'Oklahoma': 36,
-//   'Oregon': 37,
-//   'Pennsylvania': 38,
-//   'Rhode Island': 39,
-//   'South Carolina': 40,
-//   'South Dakota': 41,
-//   'Tennessee': 42,
-//   'Texas': 43,
-//   'Utah': 44,
-//   'Vermont': 45,
-//   'Virginia': 46,
-//   'Washington': 47,
-//   'West Virginia': 48,
-//   'Wisconsin': 49,
-//   'Wyoming': 50,
-//   'Puerto Rico': 51,
-//   'District of Columbia': 52,
+// class StateChart extends Component {
+//   render() {
+//     if (this.props.data) {
+//       return (
+//         <Chart
+//           options={{
+//             chart: {
+//               id: "basic-bar"
+//             },
+//             xaxis: {
+//               categories: [0, 1, 2, 3, 4, 5]
+//             }
+//           }}
+//           series={[
+//             {
+//               name: "series-1",
+//               data: this.props.data
+//             }
+//           ]}
+//           type="bar"
+//         />
+//       )
+//     } else {
+//       return
+//     }
+//   }
 // }
 
 function App() {
   const [ activePopup, setActivePopup ] = useState( null );
 
+  // const graphMap = states.map((curr) => (
+  //   <StateChart
+  //     data={[
+  //       0,
+  //       1,
+  //       3,
+  //       4,
+  //       5
+  //     ]}
+  //     type="bar"
+  //   />
+  // ))
+
   const mapPolygonColorToDensity=(density => {
-    return density > 3000000
+    return density > 100
         ? '#a50f15'
-        : density > 2000000
+        : density > 50
         ? '#de2d26'
-        : density > 1000000
+        : density > 25
         ? '#fb6a4a'
-        : density > 500000
+        : density > 5
         ? '#fc9272'
-        : density > 100000
+        : density > 1
         ? '#fcbba1'
         : '#fee5d9';
     })
-  
-  
+ 
   const style = (feature => {
       var index = popups.findIndex(obj => obj.State === feature.properties.NAME);
       // console.log(popups[index].Population)
 
     return ({
-        fillColor: mapPolygonColorToDensity(popups[index].Population),
+        fillColor: mapPolygonColorToDensity(popups[index].ProjectTotal),
         weight: 1,
         opacity: 1,
         color: 'white',
@@ -97,9 +88,24 @@ function App() {
     });
   });
 
+  // const GetGraph = (props) => {
+  //   console.log(graphMap[props.active.State])
+  //   return graphMap[props.active.State]
+  // }
   return (
     <div>
-      <div className='logo'><img src={Logo} style={{width: 200,}} alt='alt' /></div>
+      <div className='logo'>
+        <img src={Logo} style={{width: 200,}} alt='alt' />
+      </div>
+      <div className="legend font-bold text-center flex flex-col items-center">
+            <div className='text-[20px]'>LEGEND</div>(Number of Total Projects)
+            <div className='border-4 border-[#a50f15] w-[50%] mt-2'> {'>'} 100 </div>
+            <div className='border-4 border-[#de2d26] w-[50%]' >50 - 99 </div>
+            <div className='border-4 border-[#fb6a4a] w-[50%]'> 25 - 49 </div>
+            <div className='border-4 border-[#fc9272] w-[50%]'> 5 - 24 </div>
+            <div className='border-4 border-[#fcbba1] w-[50%]'> 1 - 4 </div>
+            <div className='border-4 border-[#fee5d9] w-[50%]'> ZERO </div>
+      </div>
       <MapContainer center = { [ 38, -97 ] } zoom = { 4.5 } scrollWheelZoom = { true }>
       {states && (<GeoJSON data={states} style={style}/>)}  
       <TileLayer attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
@@ -126,17 +132,46 @@ function App() {
           }}
         >
           <div>
-            <h1>{ activePopup.State }</h1>
-            <p>Types of Projects: Bridge, Roadway, Consulting</p>
-            <p>Distance Covered: 1,000,000 miles</p>
+            <div className='flex justify-center'>
+            <h1 className='text-xl font-bold border-b-2 border-[#de2d26] w-fit font-serif'>{ activePopup.State }</h1>
+            </div>
+            <div className='text-[14px] flex items-center pt-2'>Total Number of Projects:&nbsp;<section className='font-bold'> { activePopup.ProjectTotal }&nbsp;</section> projects</div>
+            <div className='text-[14px] flex items-center'>Bridge Deck Area Inspected:&nbsp;<section className='font-bold'> { activePopup.BridgeDeckArea.toLocaleString(navigator.language, { minimumFractionDigits: 0 }) }&nbsp;</section> sqft</div>
+            <div className='text-[14px] flex items-center'>Roadway Distance Inspected:&nbsp;<section className='font-bold'> { activePopup.PavementDistance.toLocaleString(navigator.language, { minimumFractionDigits: 0 }) }&nbsp;</section> miles</div>
+            {/* <div className='text-[14px]'>&nbsp;&nbsp; Infrasense has inspected over { activePopup.BridgeDeckArea.toLocaleString(navigator.language, { minimumFractionDigits: 0 }) } sqft of bridge 
+            deck and over { activePopup.PavementDistance.toLocaleString(navigator.language, { minimumFractionDigits: 0 }) } miles of paved roadway in state of { activePopup.State }</div> */}
 
+            {activePopup && (
+            
+            <div>
+              <p className='text-[15px] font-bold h-fit mt-8'>Project Distribution:</p>
+              <div className='flex justify-around font-bold text-center space-x-2'>
+              <div className='flex flex-col items-center'><section className='text-[14px]'>{activePopup.BridgeDeckScanning} </section>
+              <a href='https://infrasense.com/bridge-deck-scanning/'><img className="min-w-[55px] h-[35px] rounded-sm hover:border-[#de2d26] hover:border-2 hover:opacity-85" alt='img' src={bridgeLogo}></img></a>Bridges</div>
+              <div className='flex flex-col items-center'><section className='text-[14px]'>{activePopup.PavementStructureEvaluation}</section>
+              <a href='https://infrasense.com/pavement-structure-evaluation/'><img className="min-w-[55px] h-[35px] rounded-sm hover:border-[#de2d26] hover:border-2 hover:opacity-85" alt='img' src={pavementLogo}></img></a>Pavement Structures</div>
+              <div className='flex flex-col items-center'><section className='text-[14px]'>{activePopup.SubsurfaceUtilityEngineering}</section>
+              <a href='https://infrasense.com/services/utility-locating-mapping/'><img className="min-w-[55px] h-[35px] rounded-sm hover:border-[#de2d26] hover:border-2 hover:opacity-85 " alt='img' src={utilityLogo}></img></a>Utilities</div>
+              <div className='flex flex-col items-center'><section className='text-[14px]'>{activePopup.ConcreteStructureScanning}</section>
+              <a href='https://infrasense.com/services/concrete-structure-scanning/'><img className="min-w-[55px] h-[35px] rounded-sm hover:border-[#de2d26] hover:border-2 hover:opacity-85" alt='img' src={concreteLogo}></img></a>Concrete Structures</div>
+              <div className='flex flex-col items-center'><section className='text-[14px]'>{activePopup.ConsultingResearch}</section>
+              <a href='https://infrasense.com/services/consulting-research/'><img className="min-w-[55px] h-[35px] rounded-sm hover:border-[#de2d26] hover:border-2 hover:opacity-85" alt='img' src={consultingLogo}></img></a>Research</div>
+              
+              
+              </div>
+              <div className='pt-2 text-[13px] font-style: italic'>Other Projects: {activePopup.OtherProjects}</div>
+            </div>
+            )}
           </div>
         </Popup>
       )} 
-      
       </MapContainer>
-    </div>  
+      
+    </div>
   );
 }
 
 export default App;
+
+
+
